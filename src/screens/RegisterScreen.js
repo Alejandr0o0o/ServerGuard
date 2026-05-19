2bdcd20bcf8bfeaeb4234f437e4e3cce95f9002c
Alicia Jazmin Bacab Chi <aliciabacab95@gmail.com>
2026-05-19 16:35:57 -0600

    mejor diseño del login att Aliss

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,25 +1,75 @@
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function RegisterScreen({ navigation }) {
+
+  //estados
+  const [usuario, setUsuario] = useState('');
+  const [correo, setCorreo] = useState('');
+  const [password, setPassword] = useState('');
+
+  //funcion registrar
+  const registrarUsuario = () => {
+    //validar campos vacios
+    if(usuario.trim() === ''||correo.trim() === ''||password.trim() === ''){
+       Alert.alert(
+        'Error',
+        'Complete todos los campos'
+      );
+      return;
+    }
+    //registro correcto
+    Alert.alert(
+      'Registro exitoso'
+    );
+    //navegar al main
+    navigation.replace('Main');
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Crear Cuenta</Text>
 
-      <TextInput placeholder="Usuario" style={styles.input} />
-      <TextInput placeholder="Correo" style={styles.input} />
-      <TextInput placeholder="Contraseña" secureTextEntry style={styles.input} />
-
-      <Button 
-        title="Registrarse" 
-        onPress={() => navigation.replace('Main')} 
+      <TextInput
+        placeholder="Usuario"
+        placeholderTextColor="#9CA3AF"
+        style={styles.input}
+        value={usuario}
+        onChangeText={setUsuario}
       />
 
-      <Text 
-        style={styles.link}
-        onPress={() => navigation.navigate('Login')}
-      >
-        ¿Ya tienes cuenta? Inicia sesión
-      </Text>
+       <TextInput
+        placeholder="Correo"
+        placeholderTextColor="#9CA3AF"
+        style={styles.input}
+        value={correo}
+        onChangeText={setCorreo}
+      />
+
+      <TextInput
+        placeholder="Contraseña"
+        placeholderTextColor="#9CA3AF"
+        secureTextEntry
+        style={styles.input}
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      <TouchableOpacity
+        style={styles.registerButton}
+        onPress={registrarUsuario}>
+        <Text style={styles.registerButtonText}>
+          Registrarse
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Login')}>
+
+        <Text style={styles.link}>
+          ¿Ya tienes cuenta? Inicia sesión
+        </Text>
+
+      </TouchableOpacity>
     </View>
   );
 }
@@ -28,22 +78,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    padding: 20
+    paddingHorizontal: 30,
+    backgroundColor: '#0F172A'
   },
+
   title: {
-    fontSize: 24,
-    marginBottom: 20,
-    textAlign: 'center'
+    fontSize: 34,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    textAlign: 'center',
+    marginBottom: 40
   },
+
   input: {
+    backgroundColor: '#1E293B',
     borderWidth: 1,
-    marginBottom: 10,
-    padding: 10,
-    borderRadius: 5
+    borderColor: '#334155',
+    borderRadius: 14,
+    padding: 16,
+    marginBottom: 20,
+    color: '#FFFFFF',
+    fontSize: 16
   },
+
+  registerButton: {
+    backgroundColor: '#2563EB',
+    padding: 16,
+    borderRadius: 14,
+    alignItems: 'center',
+    marginTop: 10
+  },
+
+  registerButtonText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+
   link: {
-    marginTop: 15,
-    color: 'blue',
-    textAlign: 'center'
+    marginTop: 20,
+    textAlign: 'center',
+    color: '#60A5FA',
+    fontWeight: 'bold',
+    fontSize: 16
   }
+
 });
